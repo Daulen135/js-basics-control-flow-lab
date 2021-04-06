a4588a9de00a07c269a296ccc6fb2172923feed7
daulen <daulenz@gmail.com>
2021-04-06 10:05:41 -0500

    commit tree 7abdc21883f57b735c846d0a34d50463e4389fdb

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  
+    let result
+    if (x < 400){
+    result ='This one is on me!'
+    }
+  
+    if (x>2000){
+      result='I will gladly take your thirty bucks.'
+    }
+
+    if (x>2500){
+    result="No can do."
+    }
+
+    return result
+    }
+
+function ternaryCheckCity(city){ 
+  // Write your code here!
+  
+  return (city === "NYC")? "Ok, sounds good." : "No go.";
+  
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  /*
+  let tip;
+  switch(tip){
+  case "generous":
+    return 'Thank you so much.';
+    
+  case "not as generous":
+    return 'Thank you.';
+  default:
+    return "Bye.";
+    
+  }
 }
+*/
+
+
+if (tip === `generous`)
+  {return `Thank you so much.`}
+if (tip === `not as generous`)
+  {return `Thank you.`}
+else 
+  {return "Bye."}
+}
+
+
